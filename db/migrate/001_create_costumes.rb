cf3fc6d75b5d76a7473217815554b08d7cacbbaf
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-08 06:34:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,3 +9,15 @@
 # value types according to the spec
 
 class Costumes < ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+    
+end 
