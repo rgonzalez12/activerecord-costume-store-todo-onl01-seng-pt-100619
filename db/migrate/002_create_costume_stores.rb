89b7f586a9a647cb0304178d48c10d7901640254
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-08 06:38:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,5 +3,9 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
   
   def change
-    create_table :
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :
 
